84c32c3585d0abd9d0efe83e335a3426e2bf6a8f
s0ba <LeachA@macbook-air.berkshire.nhs.uk>
2015-11-11 15:14:36 +0000

    Added gulp-compass and compiled SCSS into css file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     gUtil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass');
 
 var coffeeSources = ['builds/dev/components/coffee/tagline.coffee'];
 
@@ -13,6 +14,8 @@ var jsSources = [
     'builds/dev/components/scripts/tagline.js',
     'builds/dev/components/scripts/template.js'
 ];
+
+var sassSources = ['builds/dev/components/sass/style.scss'];
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -27,3 +30,15 @@ gulp.task('js', function() {
         .pipe(browserify())
         .pipe(gulp.dest('builds/dev/js'))
 });
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass : 'builds/dev/components/sass',
+            image : 'builds/dev/images',
+            style : 'expanded'
+        }))
+        .pipe(gulp.dest('builds/dev/css')
+            .on('error', gUtil.log))
+
+})
